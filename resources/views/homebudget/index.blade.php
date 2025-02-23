9869c1af060d747015c2dc72a533b82e0ec5b725
makoto <akagami8130@gmail.com>
2025-02-24 02:30:07 +0900

    fix: バリデーション修正

--- resources/views/homebudget/index.blade.php
+++ resources/views/homebudget/index.blade.php
@@ -35,18 +35,18 @@
                 @csrf
                 <label for="date">日付:</label>
                 <input type="date" id="date" name="date">
-                @if($errors->has('date')) <span>{{$errors->first('date')}}</span> @endif
+                @if($errors->has('date')) <span class="error">{{$errors->first('date')}}</span> @endif
                 <label for="category">カテゴリ:</label>
                 <select name="category" id="category">
                     @foreach($categories as $category)
                     <option value="{{$category->id}}">{{$category->name}}</option>
                     @endforeach
                 </select>
-                @if($errors->has('category')) <span>{{$errors->first('category')}}</span> @endif
+                @if($errors->has('category')) <span class="error">{{$errors->first('category')}}</span> @endif
 
                 <label for="price">金額:</label>
                 <input type="text" id="price" name="price">
-                @if($errors->has('price')) <span>{{$errors->first('price')}}</span> @endif
+                @if($errors->has('price')) <span class="error">{{$errors->first('price')}}</span> @endif
 
                 <button type="submit">追加</button>
             </form>
